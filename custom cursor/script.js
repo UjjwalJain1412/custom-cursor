0e6c90e927e57cf7043867d99352559e510b688f
Ujjwal Jain <168171990+UjjwalJain1412@users.noreply.github.com>
2024-05-09 13:59:06 +0530

    Delete custom cursor directory

--- custom cursor/script.js
+++
@@ -1,7 +0,0 @@
-var main = document.querySelector("#main")
-var crsr = document.querySelector(".cursor")
-
-main.addEventListener("mousemove", function(hi){
-    crsr.style.left = hi.x+ "px"
-    crsr.style.top = hi.y+ "px"
-})
